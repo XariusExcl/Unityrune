c4835168e8e24f7bf25681d33ea335fab39355c6
Jules <s4b4t3ryoutube@outlook.fr>
2019-01-21 23:28:30 +0100

    First commit
    Pushing
    All the code!
    :crabRave:

---
+++ Assets/Scripts/RalseiController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RalseiController : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public Animator animator;
+
+	float inputHorizontal = 0f;
+	float inputVertical = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+		inputHorizontal = Input.GetAxisRaw("Horizontal");
+		inputVertical = Input.GetAxisRaw("Vertical");
+
+		animator.SetFloat("Horizontal", inputHorizontal);
+		animator.SetFloat("Vertical", inputVertical);
+	}
+
+	void FixedUpdate() {
+		rb.velocity = new Vector2(inputHorizontal, inputVertical);
+
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-UnityRune
+# UnityRune v0.0 ! 
+
+This is a recreation on Deltarune's engine in Unity (I'm using Unity2018.3.2f1, that's quite a mouthful.)
+
+This project started somewhere in January 2019, and is being worked on by me, a student in... Web development...?
+
+Alright I don't have a lot to say in this, right now nothing is working :)
